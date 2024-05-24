5085cb7e9433f9de3f9a973117bf86f09488e6bc
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-05-24 19:33:17 +0530

    Creating the register form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import RegisterForm from './RegisterForm'
 
 export default function RegisterPage() {
   return (
-    <div>
-      Register Page
+    <div className="flex items-center justify-center align-middle vertical-center">
+      <RegisterForm />
     </div>
   )
 }
