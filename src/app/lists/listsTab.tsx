652e7cc321184512bd55078d9cf3240629ff1832
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-05-27 02:13:50 +0530

    Fixed flicker effect via transition and loader

--- src/app/lists/listsTab.tsx
+++ src/app/lists/listsTab.tsx
@@ -3,8 +3,9 @@
 import { Tab, Tabs } from "@nextui-org/react";
 import { Member } from "@prisma/client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { Key } from "react";
+import { Key, useTransition } from "react";
 import MemberCard from "../members/MemberCard";
+import LoadingComponent from "@/components/LoadingComponent";
 
 type Props = {
   members: Member[];
@@ -15,6 +16,7 @@ export default function ListsTab({ members, likeIds }: Props) {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
+  const [isPending, startTransition] = useTransition();
 
   const tabs = [
     { id: "source", label: "Members I have liked" },
@@ -23,25 +25,43 @@ export default function ListsTab({ members, likeIds }: Props) {
   ];
 
   function handleTabChange(key: Key) {
-    const params = new URLSearchParams(searchParams);
-    params.set('type', key.toString());
-    router.replace(`${pathname}?${params.toString()}`);
+    startTransition(() => {
+      const params = new URLSearchParams(searchParams);
+      params.set("type", key.toString());
+      router.replace(`${pathname}?${params.toString()}`);
+    });
   }
 
   return (
     <div className="flex w-full flex-col mt-10 gap-5">
-      <Tabs aria-label="Like tabs" items={tabs} color="secondary" onSelectionChange={handleTabChange}>
+      <Tabs
+        aria-label="Like tabs"
+        items={tabs}
+        color="secondary"
+        onSelectionChange={handleTabChange}
+      >
         {(item) => (
           <Tab key={item.id} title={item.label}>
-            {members.length > 0 ? (
+            {isPending ? (
+                <LoadingComponent />
+            ):(
+                <>
+                {members.length > 0 ? (
               <div className=" grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
-                {members.map(member => (
-                  <MemberCard key={member.id} member={member} likeIds={likeIds} />
+                {members.map((member) => (
+                  <MemberCard
+                    key={member.id}
+                    member={member}
+                    likeIds={likeIds}
+                  />
                 ))}
               </div>
             ) : (
               <div>No members for this filter</div>
             )}
+                </>
+            )}
+            
           </Tab>
         )}
       </Tabs>
