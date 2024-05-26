8b76e5e2cca30bafe2090e1ec5fff54f3fc70484
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-05-27 01:56:58 +0530

    Adding the like list tabs

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -2,6 +2,8 @@
 
 import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma"
+import { Photo } from "@prisma/client";
+import { notFound } from "next/navigation";
 
 export async function getMembers() {
     const session = await auth()
@@ -27,4 +29,16 @@ export async function getMemberByUserId(userId: string){
     } catch (error) {
         console.log(error)
     }
+}
+
+export async function getMemberPhotosByUserId(userId:string) {
+    const member = await prisma.member.findUnique({
+        where:{
+            userId
+        },
+        select:{photos:true}
+    })
+
+    if(!member) return null
+    return member.photos.map(p=>p) as Photo[]
 }
