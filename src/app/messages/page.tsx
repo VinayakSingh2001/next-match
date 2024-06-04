cfc1695ce3040b71f8fa5126103c79ab6887eebb
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-06-04 06:39:03 +0530

    Adding the message sidebar

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import MessageSideBar from './MessageSideBar'
 
 export default function MessagesPage() {
   return (
-    <div>
-      messages
+    <div className='grid grid-cols-12 gap-5 h-[80vh] mt-10 '>
+     <div className='col-span-2'>
+      <MessageSideBar />
+     </div>
+     <div className="col-span-10">
+      meggase
+     </div>
     </div>
   )
 }
