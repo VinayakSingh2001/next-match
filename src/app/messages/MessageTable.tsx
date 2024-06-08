7cfb88e4e1069f5a98a6d413ec713d2e59b42fef
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-06-08 14:59:23 +0530

    Finishing up the message table

--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -13,8 +13,10 @@ import {
   getKeyValue,
 } from "@nextui-org/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { Key, useCallback } from "react";
+import React, { Key, useCallback, useState } from "react";
 import { AiFillDelete } from "react-icons/ai";
+import { deleteMessage } from "../actions/messageActions";
+import { truncateString } from "@/lib/util";
 
 type Props = {
   messages: MessageDTO[];
@@ -25,6 +27,7 @@ export default function MessageTable({ messages }: Props) {
   //by default we are working with the INBOX container
   const searchParams = useSearchParams();
   const isOutbox = searchParams.get("container") === "outbox";
+  const [isDeleting, setDeleting] = useState({ id: "", loading: false });
   const handleRowSelect = (key: Key) => {
     const message = messages.find((m) => m.id === key);
     const url = isOutbox
@@ -42,6 +45,13 @@ export default function MessageTable({ messages }: Props) {
     { key: "actions", label: "Actions" },
   ];
 
+  const handleDeleteMessage = useCallback(async(message: MessageDTO)=>{
+    setDeleting({ id: message.id, loading: true });
+    await deleteMessage(message.id, isOutbox);
+    router.refresh();
+    setDeleting({ id: "", loading: false });
+  },[isOutbox, router]) 
+
   //giving user the ablity to delete (via cusom cell) in order to use cusom cell we need a renderCell function
   const renderCell = useCallback(
     (item: MessageDTO, columnKey: keyof MessageDTO) => {
@@ -51,9 +61,7 @@ export default function MessageTable({ messages }: Props) {
         case "recipientName":
         case "senderName":
           return (
-            <div
-              className='flex items-center gap-2 cursor-pointer'
-            >
+            <div className="flex items-center gap-2 cursor-pointer">
               <Avatar
                 src={
                   (isOutbox ? item.recipientImage : item.senderImage) ||
@@ -66,18 +74,18 @@ export default function MessageTable({ messages }: Props) {
             </div>
           );
         case "text":
-          return <div className="truncate">{cellValue}</div>;
+          return <div className="truncate">{truncateString(cellValue, 100)}</div>;
         case "created":
           return cellValue;
         default:
           return (
-            <Button isIconOnly variant="light">
+            <Button isIconOnly variant="light" isLoading={isDeleting.id === item.id && isDeleting.loading} onClick={()=>handleDeleteMessage(item)}>
               <AiFillDelete size={24} className="text-danger" />
             </Button>
           );
       }
     },
-    [isOutbox]
+    [isOutbox, isDeleting.id, isDeleting.loading, handleDeleteMessage]
   );
 
   return (
@@ -92,7 +100,7 @@ export default function MessageTable({ messages }: Props) {
       >
         <TableHeader columns={columns}>
           {(column) => (
-            <TableColumn key={column.key}>{column.label}</TableColumn>
+            <TableColumn key={column.key}  width={column.key==='text'?'50%':undefined} >{column.label}</TableColumn>
           )}
         </TableHeader>
         <TableBody
@@ -102,9 +110,11 @@ export default function MessageTable({ messages }: Props) {
           {(item) => (
             <TableRow key={item.id} className="cursor-pointer">
               {(columnKey) => (
-                <TableCell className={`${
-                  !item.dateRead && !isOutbox ? "font-semibold" : ""
-                }`}>
+                <TableCell
+                  className={`${
+                    !item.dateRead && !isOutbox ? "font-semibold" : ""
+                  }`}
+                >
                   {renderCell(item, columnKey as keyof MessageDTO)}
                 </TableCell>
               )}
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -34,3 +34,11 @@ export function transformImageUrl(imageUrl?: string | null){
 export function formatShortDateTime(date: Date){
   return format(date, 'dd MMM yy h:mm:a')
 }
+
+export function truncateString(text?:string | null, num=50){
+  if(!text) return null
+  if(text.length <=num){
+    return text
+  }
+  return text.slice(0,num) + '...';
+}
