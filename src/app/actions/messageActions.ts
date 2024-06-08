5e96760f8e4dd2b3f78fd72af32ac2b18f856920
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-06-08 14:38:58 +0530

    Adding the delete message action

--- src/app/actions/messageActions.ts
+++ src/app/actions/messageActions.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 
 import { MessageSchema, messageSchema } from "@/lib/schemas/messageSchema";
 import { ActionResult } from "@/types";
@@ -8,131 +8,177 @@ import { prisma } from "@/lib/prisma";
 import { mapMessageToMessageDTO } from "@/lib/mappings";
 
 //ye ek function h jiski help se hum kisi ko message bheej sakte h
-export async function createMessage(recipientUserId:string, data: MessageSchema):Promise<ActionResult<Message>> {
-    try {
-        const userId = await getAuthUserId();
-        const validated = messageSchema.safeParse(data)
+export async function createMessage(
+  recipientUserId: string,
+  data: MessageSchema
+): Promise<ActionResult<Message>> {
+  try {
+    const userId = await getAuthUserId();
+    const validated = messageSchema.safeParse(data);
 
-        if(!validated.success) return {status:"error", error:validated.error.errors}
-        const {text} = validated.data;
-        const message  = await prisma.message.create({
-            data:{text,recipientId: recipientUserId, senderId: userId}
-        })
+    if (!validated.success)
+      return { status: "error", error: validated.error.errors };
+    const { text } = validated.data;
+    const message = await prisma.message.create({
+      data: { text, recipientId: recipientUserId, senderId: userId },
+    });
 
-        return {status:'success', data:message}
-    } catch (error) {
-        console.log(error)
-        return {status:'error', error:"something went wrong"}
-        
-    }
+    return { status: "success", data: message };
+  } catch (error) {
+    console.log(error);
+    return { status: "error", error: "something went wrong" };
+  }
 }
 
 //ye function pura converstion wth a user k sath ko database se fetch krta h, initally jo ki absurd forat m hota h (which we directly recieve from the db), then this function converts the recievied payload into a DTO format and then return that payload
-export async function getMessageThread(recipientId:string) {
-    try {
-        const userId = await getAuthUserId()
-        //setting both sides of conservation 
-        const messages = await prisma.message.findMany({
-            where:{
-                OR:[{
-                    senderId:userId, recipientId,
-                },{
-                    senderId:recipientId, 
-                    recipientId:userId
-                }]
-            },
-            //now we give some ordering to the messages thatis latest message should show on top
-            orderBy:{
-                created:"asc"
-            }, 
-            //now get some info about the sender , name , image , the userId and same for the recipient
-            select:{
-                id:true,
-                text:true,
-                created:true,
-                dateRead:true,
-                sender:{
-                    select:{
-                        userId:true,
-                        name:true,
-                        image:true
-                    }
-                },
-                recipient:{
-                    select:{
-                        userId:true,
-                        name:true,
-                        image:true
-                    }
-                }
-            }
-        })
+export async function getMessageThread(recipientId: string) {
+  try {
+    const userId = await getAuthUserId();
+    //setting both sides of conservation
+    const messages = await prisma.message.findMany({
+      where: {
+        OR: [
+          {
+            senderId: userId,
+            recipientId,
+            senderDeleted:false
+          },
+          {
+            senderId: recipientId,
+            recipientId: userId,
+            recipientDeleted:false
+          },
+        ],
+      },
+      //now we give some ordering to the messages thatis latest message should show on top
+      orderBy: {
+        created: "asc",
+      },
+      //now get some info about the sender , name , image , the userId and same for the recipient
+      select: {
+        id: true,
+        text: true,
+        created: true,
+        dateRead: true,
+        sender: {
+          select: {
+            userId: true,
+            name: true,
+            image: true,
+          },
+        },
+        recipient: {
+          select: {
+            userId: true,
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
 
-        if(messages.length > 0){
-            await prisma.message.updateMany({
-                where:{
-                    senderId: recipientId,
-                    recipientId : userId,
-                    dateRead: null,
-
-
-                }
-                , data:{dateRead: new Date()}
-            })
-        }
-
-
-        return messages.map(message=>mapMessageToMessageDTO(message))
-    } catch (error) {
-        console.log(error)
-        throw error
+    if (messages.length > 0) {
+      await prisma.message.updateMany({
+        where: {
+          senderId: recipientId,
+          recipientId: userId,
+          dateRead: null,
+        },
+        data: { dateRead: new Date() },
+      });
     }
-}
 
+    return messages.map((message) => mapMessageToMessageDTO(message));
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 //createing action for getting the messages for the container that we are interested in that is 'INBOX" ans "OUTBOX"
-export async function getMessageByContainer(container:string) {
-    try {
-        const userId = await getAuthUserId()
-        //now we need to decide what we are gonna fetch from the db, 
-        //if we are working with the outbox,then we need to get the messages where the userId is the senderId 
-        //And if we are working with the inbox, then the userId will be equal to the recipientId
+export async function getMessageByContainer(container: string) {
+  try {
+    const userId = await getAuthUserId();
+    //now we need to decide what we are gonna fetch from the db,
+    //if we are working with the outbox,then we need to get the messages where the userId is the senderId
+    //And if we are working with the inbox, then the userId will be equal to the recipientId
 
-        const selector  = container === 'outbox'?'senderId':'recipientId';
-        const messages = await prisma.message.findMany({
-            where:{
-                [selector]: userId,
-            },
-            orderBy:{
-                created:'desc'
-            },
-            select:{
-                id:true,
-                text:true,
-                created:true,
-                dateRead:true,
-                sender:{
-                    select:{
-                        userId:true,
-                        name:true,
-                        image:true
-                    }
-                },
-                recipient:{
-                    select:{
-                        userId:true,
-                        name:true,
-                        image:true
-                    }
-                }
-            }
-        })
-        return messages.map(message=>mapMessageToMessageDTO(message))
-
-
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
+    const conditions = {
+        [container === "outbox" ? "senderId" : "recipientId"]: userId,
+        ...(container === 'outbox'? {senderDeleted:false}:{recipientDeleted:false})
+      };
+    const messages = await prisma.message.findMany({
+      where: conditions,
+      orderBy: {
+        created: "desc",
+      },
+      select: {
+        id: true,
+        text: true,
+        created: true,
+        dateRead: true,
+        sender: {
+          select: {
+            userId: true,
+            name: true,
+            image: true,
+          },
+        },
+        recipient: {
+          select: {
+            userId: true,
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+    return messages.map((message) => mapMessageToMessageDTO(message));
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
+export async function deleteMessage(messageId: string, isOutbox: boolean) {
+  const userId = await getAuthUserId();
+  const selector = isOutbox ? "senderDeleted" : "recipientDeleted";
+  
+
+  try {
+    const userId = await getAuthUserId();
+    await prisma.message.update({
+      where: { id: messageId },
+      data: {
+        [selector]: true,
+      },
+    });
+
+    const messageToDelete = await prisma.message.findMany({
+      where: {
+        OR: [
+          {
+            senderId: userId,
+            senderDeleted: true,
+            recipientDeleted: true,
+          },
+          {
+            recipientId: userId,
+            senderDeleted: true,
+            recipientDeleted: true,
+          },
+        ],
+      },
+    });
+    if (messageToDelete.length > 0) {
+      await prisma.message.deleteMany({
+        where: {
+          OR: messageToDelete.map((m) => ({ id: m.id })),
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -1,23 +1,37 @@
 "use client";
-import {MessageDTO} from '@/types'
-import { Card, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, getKeyValue } from "@nextui-org/react";
+import { MessageDTO } from "@/types";
+import {
+  Avatar,
+  Button,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  getKeyValue,
+} from "@nextui-org/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { Key } from "react";
+import React, { Key, useCallback } from "react";
+import { AiFillDelete } from "react-icons/ai";
 
 type Props = {
-    messages: MessageDTO[]
-}
+  messages: MessageDTO[];
+};
 
-export default function MessageTable({messages}:Props) {
-    const router = useRouter()
+export default function MessageTable({ messages }: Props) {
+  const router = useRouter();
   //by default we are working with the INBOX container
   const searchParams = useSearchParams();
   const isOutbox = searchParams.get("container") === "outbox";
-  const handleRowSelect = (key:Key)=> {
-    const message = messages.find(m => m.id === key);
-    const url = isOutbox ? `/members/${message?.recipientId}`:`/members/${message?.senderId}`
-    router.push(url + '/chat');
-  }
+  const handleRowSelect = (key: Key) => {
+    const message = messages.find((m) => m.id === key);
+    const url = isOutbox
+      ? `/members/${message?.recipientId}`
+      : `/members/${message?.senderId}`;
+    router.push(url + "/chat");
+  };
   const columns = [
     {
       key: isOutbox ? "recipientName" : " senderName",
@@ -25,35 +39,79 @@ export default function MessageTable({messages}:Props) {
     },
     { key: "text", label: "Message" },
     { key: "created", label: isOutbox ? "Date sent" : "Date recieved" },
-    
+    { key: "actions", label: "Actions" },
   ];
 
+  //giving user the ablity to delete (via cusom cell) in order to use cusom cell we need a renderCell function
+  const renderCell = useCallback(
+    (item: MessageDTO, columnKey: keyof MessageDTO) => {
+      const cellValue = item[columnKey];
+
+      switch (columnKey) {
+        case "recipientName":
+        case "senderName":
+          return (
+            <div
+              className='flex items-center gap-2 cursor-pointer'
+            >
+              <Avatar
+                src={
+                  (isOutbox ? item.recipientImage : item.senderImage) ||
+                  "images/user.png"
+                }
+                alt="image of member"
+                className="avatar"
+              />
+              <span>{cellValue}</span>
+            </div>
+          );
+        case "text":
+          return <div className="truncate">{cellValue}</div>;
+        case "created":
+          return cellValue;
+        default:
+          return (
+            <Button isIconOnly variant="light">
+              <AiFillDelete size={24} className="text-danger" />
+            </Button>
+          );
+      }
+    },
+    [isOutbox]
+  );
+
   return (
-    <Card className='flex flex-col gap-3 h-[80vh] overflow-auto'>
-        <Table
-      aria-label="Table with messages"
-      selectionMode="single"
-      onRowAction={(key) => {handleRowSelect(key)}}
-      shadow='none'
-    >
-      <TableHeader columns={columns}>
-        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={messages} emptyContent='No Message for this container'>
-            {(item)=>(
-                <TableRow key={item.id} className='cursor-pointer'>
-                    {(columnKey)=>(
-                        <TableCell>
-                          <div className={`${!item.dateRead && !isOutbox? 'font-semibold':''}`}>
-                          {getKeyValue(item, columnKey)}
-                          </div>
-                            
-                        </TableCell>
-                    )}
-                </TableRow>
-            )}
-      </TableBody>
-    </Table>
+    <Card className="flex flex-col gap-3 h-[80vh] overflow-auto">
+      <Table
+        aria-label="Table with messages"
+        selectionMode="single"
+        onRowAction={(key) => {
+          handleRowSelect(key);
+        }}
+        shadow="none"
+      >
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          items={messages}
+          emptyContent="No Message for this container"
+        >
+          {(item) => (
+            <TableRow key={item.id} className="cursor-pointer">
+              {(columnKey) => (
+                <TableCell className={`${
+                  !item.dateRead && !isOutbox ? "font-semibold" : ""
+                }`}>
+                  {renderCell(item, columnKey as keyof MessageDTO)}
+                </TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
     </Card>
   );
 }
